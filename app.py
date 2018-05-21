3c213a802756adabe75e8f92e38dc36ffb25f7f8
ringo <617243818@qq.com>
2018-05-21 11:00:13 +0800

    2nd version

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
+
+@app.route('/detail')
+def detail():
+    return render_template('detail.html')
+
+@app.route('/mac')
+def mac():
+	print request
+	return jsonify(result=2)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
